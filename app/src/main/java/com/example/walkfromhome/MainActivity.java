ce298371c1af36c45f181c32b90d0426d59fb3c1
ZODIAC-ZD <pratipan.butsaeng@gmail.com>
2024-04-17 13:32:29 +0700

    fix back

--- app/src/main/java/com/example/walkfromhome/MainActivity.java
+++ app/src/main/java/com/example/walkfromhome/MainActivity.java
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
