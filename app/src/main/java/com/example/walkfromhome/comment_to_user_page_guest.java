d7558abd63f66f05b28cb26c9747954308b7d9ef
Kunanon <kunanon.kl@cdg.co.th>
2024-04-19 00:55:46 +0700

    complete

--- app/src/main/java/com/example/walkfromhome/comment_to_user_page_guest.java
+++ app/src/main/java/com/example/walkfromhome/comment_to_user_page_guest.java
@@ -1,29 +1,68 @@
 package com.example.walkfromhome;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.time.LocalDate;
+
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class comment_to_user_page_guest extends AppCompatActivity {
 
     private Button first_page_but;
+
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private long maxId = 0;
+    private final String day = String.valueOf(LocalDate.now().getDayOfMonth());
+    private final String month = String.valueOf(LocalDate.now().getMonthValue());
+    private final String year = String.valueOf(LocalDate.now().getYear());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment_to_user_page);
 
+        database = FirebaseDatabase.getInstance("https://walkfromhome-431de-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        myRef = database.getReference().child("Data");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    maxId = snapshot.getChildrenCount();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Failed to read value
+                Log.w("Failed to read value.", error.toException());
+            }
+        });
+
         first_page_but = findViewById(R.id.first_page__but);
-        Intent next_page = new Intent(comment_to_user_page_guest.this,show_test_result_page.class);
         Intent guest_result_page = new Intent(comment_to_user_page_guest.this,show_test_result_guest.class);
         first_page_but.setOnClickListener(new View.OnClickListener() {
             Intent next_page = new Intent(comment_to_user_page_guest.this,show_test_result_page.class);
             @Override
             public void onClick(View v) {
-                if(GlobalVariable.login_check == true){
+                save();
+                if(GlobalVariable.login_check){
                     startActivity(next_page);
                     finish();
                 }
@@ -34,5 +73,36 @@ public class comment_to_user_page_guest extends AppCompatActivity {
             }
         });
 
+    }
+    public  void save(){
+        TestResults testResults = new TestResults();
+        testResults.setFirstName(GlobalVariable.firstName);
+        testResults.setLastName(GlobalVariable.lastName);
+        testResults.setWeight(GlobalVariable.weight);
+        testResults.setHeight(GlobalVariable.height);
+        testResults.setGender(GlobalVariable.gender);
+        testResults.setStep(GlobalVariable.step);
+        testResults.setWalking_distance(GlobalVariable.walking_distance);
+        testResults.setWalking_information(GlobalVariable.walking_information);
+        testResults.setStride_length(GlobalVariable.stride_length);
+        testResults.setId(maxId + 1);
+        testResults.setHeartRate(GlobalVariable.heartRate);
+        testResults.setBloodOxygen(GlobalVariable.bloodOxygen);
+        testResults.setBloodPress(GlobalVariable.bloodPress);
+        testResults.setExhaustion(GlobalVariable.exhaustion);
+        testResults.setDate(day + "/" + month + "/" + year);
+        myRef.child(String.valueOf(maxId + 1)).setValue(testResults);
+    }
+
+    public void alert(String message){
+        AlertDialog dlg = new AlertDialog.Builder(this)
+                .setTitle("Alert").setMessage(message)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).create();
+        dlg.show();
     }
 }
--- app/src/main/java/com/example/walkfromhome/final_page_general_user.java
+++ app/src/main/java/com/example/walkfromhome/final_page_general_user.java
@@ -1,64 +1,28 @@
 package com.example.walkfromhome;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.time.LocalDate;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class final_page_general_user extends AppCompatActivity {
     private Button return_butt;
-    private FirebaseDatabase database;
-    private DatabaseReference myRef;
-    private long maxId = 0;
-    private final String day = String.valueOf(LocalDate.now().getDayOfMonth());
-    private final String month = String.valueOf(LocalDate.now().getMonthValue());
-    private final String year = String.valueOf(LocalDate.now().getYear());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_page_general_user);
-        database = FirebaseDatabase.getInstance("https://walkfromhome-431de-default-rtdb.asia-southeast1.firebasedatabase.app/");
-        myRef = database.getReference().child("Data");
-
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    maxId = (snapshot.getChildrenCount());
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                // Failed to read value
-                Log.w("Failed to read value.", error.toException());
-            }
-        });
-
-        TestResults testResults = getTestResults();
-        myRef.child(String.valueOf(maxId + 1)).setValue(testResults);
 
         return_butt = findViewById(R.id.return_button3);
         return_butt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent go_first_page = new Intent(getApplicationContext(), login_page.class);
                 startActivity(go_first_page);
                 finish();
@@ -66,24 +30,4 @@ public class final_page_general_user extends AppCompatActivity {
         });
     }
 
-    @NonNull
-    private TestResults getTestResults() {
-        TestResults testResults = new TestResults();
-        testResults.setFirstName(GlobalVariable.firstName);
-        testResults.setLastName(GlobalVariable.lastName);
-        testResults.setWeight(GlobalVariable.weight);
-        testResults.setHeight(GlobalVariable.height);
-        testResults.setGender(GlobalVariable.gender);
-        testResults.setStep(GlobalVariable.step);
-        testResults.setWalking_distance(GlobalVariable.walking_distance);
-        testResults.setWalking_information(GlobalVariable.walking_information);
-        testResults.setStride_length(GlobalVariable.stride_length);
-        testResults.setId(maxId + 1);
-        testResults.setHeartRate(GlobalVariable.heartRate);
-        testResults.setBloodOxygen(GlobalVariable.bloodOxygen);
-        testResults.setBloodPress(GlobalVariable.bloodPress);
-        testResults.setExhaustion(GlobalVariable.exhaustion);
-        testResults.setDate(day + "/" + month + "/" + year);
-        return testResults;
-    }
 }
